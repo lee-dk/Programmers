73af306c3d2f75248c7aa7ada6601cb797e2335e
DongGyuLee <dlehdrb1129@gmail.com>
2020-10-23 18:22:51 +0900

    20201023 프로그래머스 Lv1 '두 개 뽑아서 더하기'

---
+++ solution1-1.py
@@ -0,0 +1,22 @@
+# 두 개 뽑아서 더하기
+""" 문제
+    정수 배열 numbers가 주어집니다. 
+    numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 
+    더해서 만들 수 있는 모든 수를 배열에 오름차순으로 담아 
+    return 하도록 solution 함수를 완성해주세요.
+"""
+''' 제한사항
+    numbers의 길이는 2 이상 100 이하입니다.
+    numbers의 모든 수는 0 이상 100 이하입니다.
+'''
+#풀이
+def solution(numbers):
+    answer = []
+    if len(numbers) >= 2 and len(numbers) <= 100:
+        for i in range(len(numbers)-1) :
+            for j in range(i+1, len(numbers)) :
+                num = numbers[i] + numbers[j]
+                if (num not in answer) :
+                    answer.append(num)                
+    answer.sort()
+    return answer
